a02c4b8a2275fadd69de739229cd3486edce80c5
George Gale <george.gale@specto.si>
2024-02-20 19:00:28 +0100

    Topics

---
+++ receive_logs_topic.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+var args = process.argv.slice(2);
+
+if (args.length == 0) {
+    console.log("Usage: receive_logs_topic.js <facility>.<severity>");
+    process.exit(1);
+}
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var exchange = 'topic_logs';
+
+        channel.assertExchange(exchange, 'topic', {
+            durable: false
+        });
+
+        channel.assertQueue('', {
+            exclusive: true
+        }, function(error2, q) {
+            if (error2) {
+                throw error2;
+            }
+            console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+            args.forEach(function(key) {
+                channel.bindQueue(q.queue, exchange, key);
+            });
+
+            channel.consume(q.queue, function(msg) {
+                console.log(" [x] %s:'%s'", msg.fields.routingKey, msg.content.toString());
+            }, {
+                noAck: true
+            });
+        });
+    });
+});
+
+/*
+To receive all the logs:
+
+./receive_logs_topic.js "#"
+To receive all logs from the facility "kern":
+
+./receive_logs_topic.js "kern.*"
+Or if you want to hear only about "critical" logs:
+
+./receive_logs_topic.js "*.critical"
+You can create multiple bindings:
+
+./receive_logs_topic.js "kern.*" "*.critical"
+And to emit a log with a routing key "kern.critical" type:
+
+./emit_log_topic.js "kern.critical" "A critical kernel error"
+ */
